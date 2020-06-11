5957b4850b7b2c1ea63913414bcc388a7017fac8
tanisha-agrawal <47605787+tanisha-agrawal@users.noreply.github.com>
2020-06-11 15:02:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,8 +13,8 @@ resource "aws_security_group" "task1-sg" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 80
+    to_port     = 80
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
